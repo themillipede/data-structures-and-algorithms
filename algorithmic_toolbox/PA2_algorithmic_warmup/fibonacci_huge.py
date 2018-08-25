3c261046db97810510554499fe212b16ed4dbe70
Katherine Millican <katherine.e.millican@gmail.com>
2018-08-25 19:42:44 +0100

    new algorithm added

--- algorithmic_toolbox/PA2_algorithmic_warmup/fibonacci_huge.py
+++ algorithmic_toolbox/PA2_algorithmic_warmup/fibonacci_huge.py
@@ -15,19 +15,21 @@ def get_fibonacci_huge_naive(n, m):
 def get_fibonacci(n):
     if n <= 1:
         return n
-    previous = 0
-    current = 1
+    prev = 0
+    curr = 1
     for _ in range(n - 1):
-        previous, current = current, previous + current
-    return current
+        prev, curr = curr, prev + curr
+    return curr
 
 
 def get_pisano_period(m):
     pisano_period = []
     n = 2
+    prev = 0
+    curr = 1
     while pisano_period[-2:] != [0, 1]:
-        fib_n = get_fibonacci(n)
-        pisano_period.append(fib_n % m)
+        prev, curr = curr, prev + curr
+        pisano_period.append(curr % m)
         n += 1
     return len(pisano_period)
 
@@ -36,6 +38,21 @@ def get_fibonacci_huge(n, m):
     len_pisano_period = get_pisano_period(m)
     remainder = n % len_pisano_period
     return get_fibonacci(remainder) % m
+
+
+def get_fibonacci_huge_alt(n, m):
+    pisano_period = []
+    fib_numbers = [0, 1]
+    i = 2
+    prev = 0
+    curr = 1
+    while pisano_period[-2:] != [0, 1]:
+        prev, curr = curr, prev + curr
+        pisano_period.append(curr % m)
+        fib_numbers.append(curr)
+        i += 1
+    remainder = n % len(pisano_period)
+    return fib_numbers[remainder] % m
 
 
 if __name__ == '__main__':
