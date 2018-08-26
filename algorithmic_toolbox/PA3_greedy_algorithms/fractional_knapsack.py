4909b094055f88908869e15ae866ff01cf0166ad
Katherine Millican <katherine.e.millican@gmail.com>
2018-08-26 20:28:02 +0100

    new algorithm added

---
+++ algorithmic_toolbox/PA3_greedy_algorithms/fractional_knapsack.py
@@ -0,0 +1,29 @@
+# Uses python3
+import sys
+
+
+def get_optimal_value(capacity, weights, values):
+    value = 0.
+    value_per_weight = [(i, values[i] / weights[i]) for i, _ in enumerate(values)]
+    value_per_weight.sort(key=lambda x: x[1], reverse=True)
+    i = 0
+    while capacity > 0 and i < len(values):
+        j = value_per_weight[i][0]
+        if weights[j] <= capacity:
+            capacity_used = weights[j]
+            value += values[j]
+        else:
+            capacity_used = capacity
+            value += capacity * value_per_weight[i][1]
+        i += 1
+        capacity -= capacity_used
+    return value
+
+
+if __name__ == "__main__":
+    data = list(map(int, sys.stdin.read().split()))
+    n, capacity = data[0:2]
+    values = data[2:(2 * n + 2):2]
+    weights = data[3:(2 * n + 2):2]
+    opt_value = get_optimal_value(capacity, weights, values)
+    print("{:.10f}".format(opt_value))
