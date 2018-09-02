8ef2118fb4e9cba8107d6f80e28d4e2acb2550f3
Katherine Millican <katherine.e.millican@gmail.com>
2018-09-02 14:08:27 +0200

    new algorithm added

--- data_structures/PA1_basic_data_structures/check_brackets.py
+++ data_structures/PA1_basic_data_structures/check_brackets.py
@@ -1,5 +1,6 @@
 import sys
 
+
 class Bracket:
     def __init__(self, bracket_type, position):
         self.bracket_type = bracket_type
@@ -14,17 +15,25 @@ class Bracket:
             return True
         return False
 
+
 if __name__ == "__main__":
     text = sys.stdin.read()
 
     opening_brackets_stack = []
+    result = None
     for i, next in enumerate(text):
         if next == '(' or next == '[' or next == '{':
-            # Process opening bracket, write your code here
-            pass
-
+            bracket = Bracket(next, i + 1)
+            opening_brackets_stack.append(bracket)
         if next == ')' or next == ']' or next == '}':
-            # Process closing bracket, write your code here
-            pass
-
-    # Printing answer, write your code here
+            if opening_brackets_stack:
+                prev = opening_brackets_stack.pop()
+                if not prev.Match(next):
+                    result = i + 1
+                    break
+            else:
+                result = i + 1
+                break
+    if not result:
+        result = opening_brackets_stack[0].position if opening_brackets_stack else 'Success'
+    print(result)
