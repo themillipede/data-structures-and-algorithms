da27436dd1f361fbad1eca41eb7dc35fd2a26b94
Katherine Millican <katherine.e.millican@gmail.com>
2018-09-03 01:50:33 +0100

    new algorithm added

---
+++ algorithmic_toolbox/PA3_greedy_algorithms/different_summands.py
@@ -0,0 +1,23 @@
+# Uses python3
+import sys
+
+
+def optimal_summands(n):
+    summands = []
+    total = 0
+    count = 1
+    while n - total >= count:
+        summands.append(count)
+        total += count
+        count += 1
+    summands[-1] += (n - total)
+    return summands
+
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    n = int(input)
+    summands = optimal_summands(n)
+    print(len(summands))
+    for x in summands:
+        print(x, end=' ')
