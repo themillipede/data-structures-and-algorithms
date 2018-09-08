36d8036d9d3d5feda5464fd0c8d07e47ccd9c8ee
Katherine Millican <katherine.e.millican@gmail.com>
2018-09-08 17:55:01 +0100

    new algorithm added

---
+++ data_structures/PA4_binary_search_trees/tree-orders.py
@@ -0,0 +1,64 @@
+# python3
+
+import sys, threading
+sys.setrecursionlimit(10**6) # max depth of recursion
+threading.stack_size(2**27)  # new thread will get stack of such size
+
+
+class TreeOrders:
+    def read(self):
+        self.n = int(sys.stdin.readline())
+        self.key = [0 for i in range(self.n)]
+        self.left = [0 for i in range(self.n)]
+        self.right = [0 for i in range(self.n)]
+        for i in range(self.n):
+            [a, b, c] = map(int, sys.stdin.readline().split())
+            self.key[i] = a
+            self.left[i] = b
+            self.right[i] = c
+
+
+    def inOrder(self):
+        self.result = []
+        def in_order_traversal(root_index):
+            if self.left[root_index] != -1:
+                in_order_traversal(self.left[root_index])
+            self.result.append(self.key[root_index])
+            if self.right[root_index] != -1:
+                in_order_traversal(self.right[root_index])
+        in_order_traversal(0)
+        return self.result
+
+
+    def preOrder(self):
+        self.result = []
+        def pre_order_traversal(root_index):
+            self.result.append(self.key[root_index])
+            if self.left[root_index] != -1:
+                pre_order_traversal(self.left[root_index])
+            if self.right[root_index] != -1:
+                pre_order_traversal(self.right[root_index])
+        pre_order_traversal(0)
+        return self.result
+
+
+    def postOrder(self):
+        self.result = []
+        def post_order_traversal(root_index):
+            if self.left[root_index] != -1:
+                post_order_traversal(self.left[root_index])
+            if self.right[root_index] != -1:
+                post_order_traversal(self.right[root_index])
+            self.result.append(self.key[root_index])
+        post_order_traversal(0)
+        return self.result
+
+
+def main():
+	tree = TreeOrders()
+	tree.read()
+    print(" ".join(str(x) for x in tree.inOrder()))
+    print(" ".join(str(x) for x in tree.preOrder()))
+    print(" ".join(str(x) for x in tree.postOrder()))
+
+threading.Thread(target=main).start()
