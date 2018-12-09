1c62904349565cd0d765b9cfc6d20da8ce1fd5f9
Katherine Millican <katherine.e.millican@gmail.com>
2018-12-09 14:30:08 +0000

    corrected mistake in algorithm

--- algorithmic_toolbox/PA6_dynamic_programming2/knapsack.py
+++ algorithmic_toolbox/PA6_dynamic_programming2/knapsack.py
@@ -6,9 +6,10 @@ def optimal_weight_alternative(capacity, item_weights):
     num_items = len(item_weights)
     value = [[0 for _ in range(num_items + 1)] for _ in range(capacity + 1)]
     for i in range(1, num_items + 1):
+        next_item_weight = item_weights[i - 1]
         for w in range(1, capacity + 1):
             prev = value[w][i - 1]
-            curr = item_weights[i - 1] + value[capacity - item_weights[i - 1]][i - 1]
+            curr = value[w - next_item_weight][i - 1] + next_item_weight
             value[w][i] = prev if curr > w else max(prev, curr)
     return value[-1][-1]
 
@@ -17,11 +18,12 @@ def optimal_weight(capacity, item_weights):
     num_items = len(item_weights)
     value = [[0 for _ in range(num_items + 1)] for _ in range(capacity + 1)]
     for i in range(1, num_items + 1):
+        next_item_weight = item_weights[i - 1]
         for w in range(1, capacity + 1):
             value[w][i] = value[w][i - 1]
-            if item_weights[i - 1] < w:
-                val = value[capacity - item_weights[i - 1]][i - 1] + item_weights[i - 1]
-                if value[w][i] < val:
+            if next_item_weight <= w:
+                val = value[w - next_item_weight][i - 1] + next_item_weight
+                if val > value[w][i]:
                     value[w][i] = val
     return value[-1][-1]
 
