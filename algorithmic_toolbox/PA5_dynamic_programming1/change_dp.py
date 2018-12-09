81dc21935ac3221120af01d9e6ec3231000d99ae
Katherine Millican <katherine.e.millican@gmail.com>
2018-12-09 23:53:51 +0000

    new algorithm added

--- algorithmic_toolbox/PA5_dynamic_programming1/change_dp.py
+++ algorithmic_toolbox/PA5_dynamic_programming1/change_dp.py
@@ -2,21 +2,33 @@
 import sys
 
 
+# Bad in terms of time and memory.
+def get_change_recursive(m):
+    if m < 1:
+        return float('inf')
+    if m in (4, 3, 1):
+        return 1
+    return 1 + min(
+        get_change_recursive(m - 4),
+        get_change_recursive(m - 3),
+        get_change_recursive(m - 1))
+
+
+# Relatively efficient in terms of time, but bad in terms of memory.
 def get_change_recursive_dp(m, min_num_coins=None):
     if not min_num_coins:
-        min_num_coins = {0: 0}
+        min_num_coins = {}
     if m in min_num_coins:
         return min_num_coins[m]
     if m < 1:
         return float('inf')
-    elif m in (4, 3, 1):
+    if m in (4, 3, 1):
         return 1
-    else:
-        min_num_coins[m] = 1 + min(
-            get_change_recursive_dp(m - 4, min_num_coins),
-            get_change_recursive_dp(m - 3, min_num_coins),
-            get_change_recursive_dp(m - 1, min_num_coins))
-        return min_num_coins[m]
+    min_num_coins[m] = 1 + min(
+        get_change_recursive_dp(m - 4, min_num_coins),
+        get_change_recursive_dp(m - 3, min_num_coins),
+        get_change_recursive_dp(m - 1, min_num_coins))
+    return min_num_coins[m]
 
 
 def get_change(money, min_num_coins=None):
@@ -30,7 +42,6 @@ def get_change(money, min_num_coins=None):
                 if num_coins < min_num_coins[m]:
                     min_num_coins[m] = num_coins
     return min_num_coins[money]
-
 
 
 if __name__ == '__main__':
