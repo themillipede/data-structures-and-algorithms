85b4e5801a3ee6973c4d12ccb45778b69aa04ef9
Katherine Millican <katherine.e.millican@gmail.com>
2019-01-14 17:53:31 +0000

    new algorithm added

--- advanced_algorithms_and_complexity/PA3_NP_completeness/budget_allocation.py
+++ advanced_algorithms_and_complexity/PA3_NP_completeness/budget_allocation.py
@@ -1,17 +1,29 @@
 # python3
+
+from itertools import product
+import numpy as np
+
 n, m = list(map(int, stdin.readline().split()))
 A = []
 for i in range(n):
-  A += [list(map(int, stdin.readline().split()))]
+    A += [list(map(int, stdin.readline().split()))]
 b = list(map(int, stdin.readline().split()))
 
-# This solution prints a simple satisfiable formula
-# and passes about half of the tests.
-# Change this function to solve the problem.
-def printEquisatisfiableSatFormula():
-    print("3 2")
-    print("1 2 0")
-    print("-1 -2 0")
-    print("1 -2 0")
 
-printEquisatisfiableSatFormula()
+def print_equisatisfiable_sat_formula():
+    result = []
+    for i, ineq in enumerate(A):
+        non_zeros = [j for j, coef in enumerate(ineq) if coef != 0]
+        for variable_assignment in product([0, 1], repeat=len(non_zeros)):
+            idx_to_var = {idx: var for idx, var in zip(non_zeros, variable_assignment)}
+            solution = [idx_to_var[k] if k in idx_to_var else 0 for k in range(len(ineq))]
+            if not np.dot(np.array(ineq), np.array(solution)) <= b[i]:
+                result.append([(-1) * ineq[j] if idx_to_var[j] == 1 else ineq[j] for j in non_zeros])
+            else:
+                result.append([ineq[j] for j in non_zeros])
+    print("%s %s" % (len(result), len(b)))
+    for clause in result:
+        print(" ".join([str(i) for i in clause]) + " 0")
+
+
+print_equisatisfiable_sat_formula()
