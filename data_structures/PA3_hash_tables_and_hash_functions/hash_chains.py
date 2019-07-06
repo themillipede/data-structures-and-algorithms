4ce56e28538b52492cba9b57dcedf55a386a2fbb
Katie Millican <katie@millican.org>
2019-07-06 02:04:48 +0100

    Descriptions added and structural improvements made

--- data_structures/PA3_hash_tables_and_hash_functions/hash_chains.py
+++ data_structures/PA3_hash_tables_and_hash_functions/hash_chains.py
@@ -1,66 +1,63 @@
 # python3
 
+# 2. Hashing with chains
+# Task: Implement a hash table with lists chaining, using the following hash function (m is the bucket count):
+#     h(S) = (SUM( S[i]x^i mod p )) mod m, where S[i] = ASCII code of i-th symbol of S, p = 1000000007, x = 263.
+#     The program should support the following kinds of queries:
+#     - add [string]: insert [string] into the table. If [string] already exists in the table, ignore the query.
+#     - del [string]: remove [string] from the table. if there is no such string in the table, ignore the query.
+#     - find [string]: output "yes" or "no" depending on whether or not the table contains [string].
+#     - check [i]: output the content of the i-th list in the table. Use spaces to separate the elements of the
+#       list. If the i-th list is empty, output a blank line.
+#     When inserting a new string into a hash chain, insert it in the beginning of the chain.
+# Input: The first line contains a single integer m  -- the number of buckets. The next line contains the number
+#     of queries N. It is followed by N lines, each containing one query in the format described above.
+# Constraints: 1 <= N <= 10^5; N/5 <= m <= N. All the strings consist of latin letters, are non-empty, and have
+#      length at most 15.
+# Output: Print the result of each find and check query, one per line, in the same order as given in the input.
+# Time limit: 7 seconds
+# Memory limit: 512 Mb
+
+
+MULTIPLIER = 263
+PRIME = 1000000007
+
 
 class Query:
-
     def __init__(self, query):
         self.type = query[0]
         if self.type == 'check':
-            self.ind = int(query[1])
+            self.idx = int(query[1])
         else:
-            self.s = query[1]
+            self.string = query[1]
 
 
-class QueryProcessor:
-    _multiplier = 263
-    _prime = 1000000007
+def hash_function(string, bucket_count):
+    ans = 0
+    for c in reversed(string):
+        ans = (ans * MULTIPLIER + ord(c)) % PRIME
+    return ans % bucket_count
 
-    def __init__(self, bucket_count):
-        self.bucket_count = bucket_count
-        self.elems = [[] for _ in range(bucket_count)]
 
-    def _hash_func(self, s):
-        ans = 0
-        for c in reversed(s):
-            ans = (ans * self._multiplier + ord(c)) % self._prime
-        return ans % self.bucket_count
-
-    def write_search_result(self, was_found):
-        print('yes' if was_found else 'no')
-
-    def write_chain(self, chain):
-        if chain:
-            print(' '.join(chain))
-        else:
-            print(' ')
-
-    def read_query(self):
-        return Query(input().split())
-
-    def process_query(self, query):
-        if query.type == "check":
-            self.write_chain(self.elems[query.ind])
-        else:
-            hash_key = self._hash_func(query.s)
-            if query.type == 'find':
-                if query.s in self.elems[hash_key]:
-                    self.write_search_result(True)
-                else:
-                    self.write_search_result(False)
-            elif query.type == 'add':
-                if query.s not in self.elems[hash_key]:
-                    self.elems[hash_key].insert(0, query.s)
-            else:
-                if query.s in self.elems[hash_key]:
-                    self.elems[hash_key].remove(query.s)
-
-    def process_queries(self):
-        n = int(input())
-        for i in range(n):
-            self.process_query(self.read_query())
+def process_query(query, hash_table):
+    if query.type == "check":
+        chain = hash_table[query.idx]
+        print(' '.join(chain)) if chain else print(' ')
+    else:
+        hash_key = hash_function(query.string, bucket_count)
+        if query.type == 'find':
+            print('yes') if query.string in hash_table[hash_key] else print('no')
+        elif query.type == 'add':
+            if query.string not in hash_table[hash_key]:
+                hash_table[hash_key].insert(0, query.string)
+        elif query.type == 'del':
+            if query.string in hash_table[hash_key]:
+                hash_table[hash_key].remove(query.string)
 
 
 if __name__ == '__main__':
     bucket_count = int(input())
-    proc = QueryProcessor(bucket_count)
-    proc.process_queries()
+    hash_table = [[] for _ in range(bucket_count)]
+    n = int(input())
+    for i in range(n):
+        process_query(Query(input().split()), hash_table)
--- data_structures/PA3_hash_tables_and_hash_functions/phone_book.py
+++ data_structures/PA3_hash_tables_and_hash_functions/phone_book.py
@@ -1,5 +1,22 @@
 # python3
 
+# 1. Phone book
+# Task: Implement a simple phone book manager. It should be able to process the following types of user queries:
+#     - add [number] [name]: Add a person with name [name] and phone number [number] to the phone book. If there
+#       exists a user with the number already, then overwrite the corresponding name.
+#     - del [number]: Erase anyone with number [number] from the phone book. If there is no such person, ignore.
+#     - find [number]: Look for a person with phone number [number] and reply with the appropriate name, or with
+#       string "not found" if there is no such person.
+# Input: The first line contains a single integer n -- the number of queries. It is followed by n lines, each of
+#     which contains one query in the format described above.
+# Constraints: 1 <= n <= 10^5. All phone numbers consist of decimal digits. They have no leading zeros, and have
+#     no more than 7 digits. All names are non-empty strings of latin letters, and have length at most 15. It is
+#     guaranteed that there is no person with name "not found".
+# Output: Print the result of each "find" query -- the name corresponding to the phone number, or "not found" if
+#     there is no person with the number, with one result per line in the same order as the find queries appear.
+# Time limit: 6 seconds
+# Memory limit: 512 Mb
+
 
 class Query:
     def __init__(self, query):
@@ -7,15 +24,6 @@ class Query:
         self.number = int(query[1])
         if self.type == 'add':
             self.name = query[2]
-
-
-def read_queries():
-    n = int(input())
-    return [Query(input().split()) for i in range(n)]
-
-
-def write_responses(result):
-    print('\n'.join(result))
 
 
 def process_queries(queries):
@@ -33,4 +41,7 @@ def process_queries(queries):
 
 
 if __name__ == '__main__':
-    write_responses(process_queries(read_queries()))
+    n = int(input())
+    queries = [Query(input().split()) for i in range(n)]
+    result = process_queries(queries)
+    print('\n'.join(result))
