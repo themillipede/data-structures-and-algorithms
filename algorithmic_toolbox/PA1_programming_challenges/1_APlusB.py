9b341deda430494321ec08198fa54ca58f509047
Katherine Millican <katherine.e.millican@gmail.com>
2019-09-16 23:09:19 +0100

    Comments added

---
+++ algorithmic_toolbox/PA1_programming_challenges/1_APlusB.py
@@ -0,0 +1,22 @@
+# python3
+
+"""
+1. Sum of two digits (SIMPLE PROBLEM TO SHOW SUBMISSION FORMAT)
+
+Task: Compute the sum of two single digit numbers.
+
+Input: Integers a and b on the same line (separated by a space).
+
+Constraints: 0 <= a, b <= 9.
+
+Output: The sum of a and b
+"""
+
+
+def sum_of_two_digits(first_digit, second_digit):
+    return first_digit + second_digit
+
+
+if __name__ == '__main__':
+    a, b = map(int, input().split())
+    print(sum_of_two_digits(a, b))
--- algorithmic_toolbox/PA1_programming_challenges/2_max_pairwise_product.py
+++ algorithmic_toolbox/PA1_programming_challenges/2_max_pairwise_product.py
@@ -1,15 +1,31 @@
 # python3
 
+"""
+2. Maximum pairwise product
 
+Task: Find the maximum product of two distinct numbers in a sequence of non-negative integers. Concretely,
+    given a sequence of non-negative integers a_1, ..., a_n, compute MAX[1 <= i != j <= n] (a_i * a_j).
+
+Input: The first line contains an integer n. The next line contains n non-negative integers a_1, ..., a_n
+    (separated by spaces).
+
+Constraints: 2 <= n <= 2*10^5; 0 <= a_1, ..., a_n <= 2*10^5.
+
+Output: The maximum pairwise product.
+"""
+
+
+# Naive, inefficient algorithm.
 def max_pairwise_product_naive(numbers):
     n = len(numbers)
     max_product = 0
-    for first in range(n):
+    for first in range(n - 1):
         for second in range(first + 1, n):
             max_product = max(max_product, numbers[first] * numbers[second])
     return max_product
 
 
+# More efficient algorithm.
 def max_pairwise_product(numbers):
     n = len(numbers)
     index = 0
