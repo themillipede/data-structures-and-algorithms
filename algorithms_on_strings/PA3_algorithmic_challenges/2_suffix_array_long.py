d23d337a932e814f44473cf14bfd0e39bc14a2ae
Katherine Millican <katherine.e.millican@gmail.com>
2019-09-22 14:30:12 +0100

    Files renamed: earlier commits will not be visible

--- algorithms_on_strings/PA3_algorithmic_challenges/2_suffix_array_long.py
+++ algorithms_on_strings/PA3_algorithmic_challenges/2_suffix_array_long.py
@@ -74,10 +74,9 @@ def update_classes(new_order, char_class, l):
 
 def build_suffix_array(text):
     """
-    Build suffix array of the string text and return a list result of
-    the same length as the text such that the value result[i] is the
-    index (0-based) in text where the i-th lexicographically smallest
-    suffix of text starts.
+    Build suffix array of the string text and return a list result of the same
+    length as the text such that the value result[i] is the index (0-based) in
+    text where the i-th lexicographically smallest suffix of text starts.
     """
     order = sort_characters(text)
     char_class = compute_char_classes(text, order)
--- algorithms_on_strings/PA3_algorithmic_challenges/4_suffix_tree_from_array.py
+++ algorithms_on_strings/PA3_algorithmic_challenges/4_suffix_tree_from_array.py
@@ -1,4 +1,5 @@
 # python3
+
 import sys
 
 
