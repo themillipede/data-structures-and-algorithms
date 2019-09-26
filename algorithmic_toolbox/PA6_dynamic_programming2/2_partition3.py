05f3f5c596d9190b9e9c72bcfebd5ae165fe4c1c
Katherine Millican <katherine.e.millican@gmail.com>
2019-09-27 00:36:48 +0100

    More comments added

--- algorithmic_toolbox/PA6_dynamic_programming2/2_partition3.py
+++ algorithmic_toolbox/PA6_dynamic_programming2/2_partition3.py
@@ -27,7 +27,9 @@ def partition3(item_weights):
         return 0
     subset_sum = int(subset_sum)
 
+    # Indices for the items, subset 1, and subset 2 (subset 3 is automatically defined based on subsets 1 and 2).
     value = [[[False for _ in range(subset_sum + 1)] for _ in range(subset_sum + 1)] for _ in range(num_items + 1)]
+    # Having considered none of the items yet for subsets 1 and 2, a division is possible (i.e. subset 3 takes all).
     value[0][0][0] = True
 
     for i in range(1, num_items + 1):
@@ -37,9 +39,16 @@ def partition3(item_weights):
                 # If a point (x, y) on the plane specified by index i has value "True", this means that it is
                 # possible to form two sets that sum to the values x and y, respectively, using only items in
                 # item_weights that have index <= i.
+
+                # x is the current size of subset 1, y is the current size of subset 2, and we want to find out
+                # if it is possible to partition this particular set of items into the three subsets. For this,
+                # we take the current item, and for the current subset sizes, determine whether it can fit into
+                # any of the three subsets, without any other items being discarded.
                 value[i][x][y] = ((value[i - 1][x - next_item_weight][y] and next_item_weight <= x) or
                                   (value[i - 1][x][y - next_item_weight] and next_item_weight <= y) or
-                                  value[i - 1][x][y])
+                                  value[i - 1][x][y]) # The item goes in subset 3.
+                # Note that the size of subset 3 is always the sum of all item weights minus (x + y).
+
     return int(value[num_items][subset_sum][subset_sum])
 
 
